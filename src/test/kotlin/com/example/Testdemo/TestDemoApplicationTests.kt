02dc6634aa381480d74b738b3a91da14536fd69b
lizeyang1995 <424011527@qq.com>
2020-12-21 14:57:57 +0800

    [lizeyang] feat: add anther fun, and verify it

--- src/test/kotlin/com/example/Testdemo/TestDemoApplicationTests.kt
+++ src/test/kotlin/com/example/Testdemo/TestDemoApplicationTests.kt
@@ -1,7 +1,6 @@
 package com.example.Testdemo
 
-import io.mockk.every
-import io.mockk.mockk
+import io.mockk.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
@@ -16,10 +15,12 @@ class TestDemoApplicationTests {
 		val mother = mockk<Mother>()
 		val kid = Kid(mother)
 		every {mother.giveMoney()} returns 30
+		every {mother.inform(any())} just Runs
 
 		kid.wantMoney()
 
 		assertEquals(30, kid.money)
+		verify {mother.inform(any())}
 	}
 
 }
